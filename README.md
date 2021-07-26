83d0f62134ed2681029ba29445eff34b1e6e8e79
IgorAvilaPereira <igor.pereira@riogrande.ifrs.edu.br>
2021-07-26 17:26:00 -0300

    codigo

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-# wdev-site
+# Instalar node, npm, vue e axios
+
+
+# Vídeos Tutoriais
+
+* https://www.youtube.com/watch?v=kmsM_hHrDHE
+
+* https://www.youtube.com/watch?v=M80y-fPHmWM
+
+* https://www.youtube.com/watch?v=vYTYZW0pYaM
+
+# Criar projeto
+```sh
+vue create wdev-site
+```
+
+# Rodar Projeto
+```sh
+npm run serve
+```
+
+# Instalar axios
+
+```sh
+npm install axios
+```
+
+
+
+
---
+++ src/components/Footer.vue
@@ -0,0 +1,55 @@
+<template>
+    <footer>
+        <a target="__blank" class="socialLink" v-for="(socialLink, index) in socialLinks" :key="index" :href="socialLink.link">
+            <img :alt="socialLink.label" :title="socialLink.title" :src="socialLink.icon">
+        </a>
+    </footer>
+</template>
+
+
+<script>
+import api from '@/services/api.js';
+
+export default {
+    name: 'Footer',
+    data() {
+        return {
+            socialLinks:[]
+        }
+    },
+    mounted(){
+        api.get('/social-links.json').then(response => {
+            // console.log(response.data);
+            this.socialLinks = response.data;
+        });
+    }
+}
+</script>
+
+<style scoped>
+footer {
+    width: 100%;
+    height: 60px;
+    background-color: var(--color-background-nav);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.socialLink {
+    border: 2px solid var(--color-text-light);
+    align-items: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 50%;
+    width: 40px;
+    height: 40px;
+    margin: 0 5px;
+
+}
+
+.socialLink img{
+    width: 20px;
+}
+
+</style>
